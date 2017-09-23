bb78fd73b64b80f4565a7108962749d225de55a9
Enzo Schettino <vincenzo.schettino@gmail.com>
2017-09-23 19:30:29 +0200

    theme adjustment

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Description of DashboardController
+ *
+ * @author Enzo
+ */
+class DashboardController extends Controller{
+    //put your code here
+    
+    public function initView(){
+        return view('admin.default');
+    }
+    
+}
